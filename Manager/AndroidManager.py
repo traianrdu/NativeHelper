c5a224c49bb9dbd4ef621a60533b1e05bd21aef1
Traian <traian.plosca@gmail.com>
2022-08-09 10:48:29 +0300

    add: Android strings.xml documentation

--- Manager/AndroidManager.py
+++ Manager/AndroidManager.py
@@ -13,7 +13,102 @@ class AndroidManager:
         -------
         convert_to_iOS()
             Converts Android string format to iOS Localizable
+
+        string.xml format
+        -------
+        <?xml version="1.0" encoding="utf-8"?>
+        <resources>
+            <plurals name="plural_name">
+                <item quantity=["zero" | "one" | "two" | "few" | "many" | "other"]>text_string</item>
+                ---
+                <item quantity="one">%d song found.</item>
+                <item quantity="other">%d songs found.</item>
+                ---
+                <item quantity="one">Znaleziono %d piosenkę.</item>
+                <item quantity="few">Znaleziono %d piosenki.</item>
+                <item quantity="other">Znaleziono %d piosenek.</item>
+                ...
+            </plurals>
+            ...
+            <string-array name="planets_array">
+                <item>Mercury</item>
+                <item>Venus</item>
+                <item>Earth</item>
+                <item>Mars</item>
+            </string-array>
+            ...
+            <string name="string_array_name">text_string</string>
+            <string name="welcome_messages">Hello, %1$s! You have %2$d new messages.</string>
+            <string name="welcome">Welcome to <b>Android</b>!</string>
+            ...
+        </resources>
+
+        Info
+        -------
+        Plurals:
+        zero	When the language requires special treatment of the number 0 (as in Arabic).
+        one	    When the language requires special treatment of numbers like one (as with the number 1 in English and
+                most other languages; in Russian, any number ending in 1 but not ending in 11 is in this class).
+        two	    When the language requires special treatment of numbers like two (as with 2 in Welsh, or 102
+                in Slovenian).
+        few	    When the language requires special treatment of "small" numbers (as with 2, 3, and 4 in Czech;
+                or numbers ending 2, 3, or 4 but not 12, 13, or 14 in Polish).
+        many	When the language requires special treatment of "large" numbers (as with numbers ending 11-99 in
+                Maltese).
+        other	When the language does not require special treatment of the given quantity (as with all
+                numbers in Chinese, or 42 in English).
+
+        Special characters:
+        Character                       Escaped form
+        @	                            \@
+        ----------------------------------------------------------------------------------------------------------------
+        ?	                            \?
+        ----------------------------------------------------------------------------------------------------------------
+        New line	                    \n
+        ----------------------------------------------------------------------------------------------------------------
+        Tab	                            \t
+        ----------------------------------------------------------------------------------------------------------------
+        U+XXXX Unicode character	    \uXXXX
+        ----------------------------------------------------------------------------------------------------------------
+        Single quote (')                Any of the following:
+                                            \'
+                                            Enclose the entire string in double quotes ("This'll work", for example)
+        ----------------------------------------------------------------------------------------------------------------
+        Double quote (")	            \"
+                                        Note that surrounding the string with single quotes does not work.
+
+        Formatting strings:
+        %1$s is a string
+        %2$d is a decimal number
+
+        HTML markup:
+        Bold: <b>, <em>
+        Italic: <i>, <cite>, <dfn>
+        25% larger text: <big>
+        20% smaller text: <small>
+        Setting font properties: <font face=”font_family“ color=”hex_color”>. Examples of possible font families include
+                                 monospace, serif, and sans_serif.
+        Setting a monospace font family: <tt>
+        Strikethrough: <s>, <strike>, <del>
+        Underline: <u>
+        Superscript: <sup>
+        Subscript: <sub>
+        Bullet points: <ul>, <li>
+        Line breaks: <br>
+        Division: <div>
+        CSS style: <span style=”color|background_color|text-decoration”>
+        Paragraphs: <p dir=”rtl | ltr” style=”…”>
+
+        Styling with annotations:
+        // values/strings.xml
+        <string name="title">Best practices for <annotation font="title_emphasis">text</annotation> on Android</string>
+
+        // values-es/strings.xml
+        <string name="title"><annotation font="title_emphasis">Texto</annotation> en Android: mejores prácticas</string>
+
+        More info:  https://developer.android.com/guide/topics/resources/string-resource
         """
+
     def __init__(self, strings_xml):
         """
         Parameters
